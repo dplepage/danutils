bcc361f9bccc36b92ad5a3ee986652aa8bba2c71
Daniel Lepage <dplepage@gmail.com>
2010-10-07 16:33:24 -0400

    Fix dtype bug

--- img/imagedb/imagedb.py
+++ img/imagedb/imagedb.py
@@ -16,14 +16,14 @@ imagedb_so = getlib(here,'imagedb_wrap.so')
 imagedb_so.imagedb_wrap.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
 
 byte_types = {
-    np.int8 : "8",
-    np.int16 : "16",
-    np.int32 : "32",
-    np.uint8 : "8",
-    np.uint16 : "16",
-    np.uint32 : "32",
-    np.float32 : "32f",
-    np.float64 : "64f"
+    np.dtype("int8") : "8",
+    np.dtype("int16") : "16",
+    np.dtype("int32") : "32",
+    np.dtype("uint8") : "8",
+    np.dtype("uint16") : "16",
+    np.dtype("uint32") : "32",
+    np.dtype("float32") : "32f",
+    np.dtype("float64") : "64f"
 }
 
 field_types = {
@@ -53,11 +53,9 @@ def imagedb(image, title = None, frame = None, fields = None, flip = 1, **kwargs
     Options to imagedb are taken from the numpy.ndarray image.
     title - title to use
     '''
-    # TODO rather than copy the array, extract the stride info and pass it
-    # into row skip and col skip parameters.
     imagedb_prog = _ensure_imagedb()
     h, w, d = np.atleast_3d(image).shape
-    b = byte_types[image.dtype.type]
+    b = byte_types[image.dtype]
     f = fields if fields is not None else field_types[d]
     t = title if title is not None else "Untitled"
     fr = frame if frame is not None else ""
@@ -65,4 +63,8 @@ def imagedb(image, title = None, frame = None, fields = None, flip = 1, **kwargs
                      "pg={imagedb_prog}").format(**locals()) 
     format_string += ' '.join(["{0}={1}".format(k,v) for k,v in kwargs.iteritems()])
     if flip: format_string += " flip"
-    imagedb_so.imagedb_wrap(image.copy().ctypes.data, format_string)
+    # TODO rather than copy the array, extract the stride info and pass it
+    # into row skip and col skip parameters.
+    x = np.ascontiguousarray(image)
+    imagedb_so.imagedb_wrap(x.ctypes.data, format_string)
+    return x
