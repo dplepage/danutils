d280a08a5a5d60fc0cfe120c9f4cbaa8e7b925ce
Daniel Lepage <dplepage@gmail.com>
2011-11-08 19:06:13 -0500

    Prevent cleaning up __main__.

--- danutils/do/cleanup.py
+++ danutils/do/cleanup.py
@@ -1,7 +1,14 @@
+class WipingMainException(Exception): pass
+
 class Clear(object):
   def __init__(self, depth=1):
     import sys
     name = sys._getframe(depth).f_globals["__name__"]
+    if name == '__main__':
+      raise WipingMainException(
+        "Importing "+__name__+" from your main module will erase"
+        " __main__ from sys.modules. This is almost certainly not"
+        " something you want to do.")
     sys.modules[name] = self
     self.name = name
   
